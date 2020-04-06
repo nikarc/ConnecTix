0ff17905e364cec5b2fbffb65352a44caf277029
Nicholas Arcuri <narcuri1@gmail.com>
2020-04-06 16:39:20 -0400

    Cart page cleanup/styling

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,44 +1,46 @@
-import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { selectTickets, removeTicket } from '../store/ticketsSlice';
-import { DateFormat } from '../utils/dates';
+import React, { useState } from 'react';
+import { useSelector } from 'react-redux';
+import { selectTickets } from '../store/ticketsSlice';
+
+import CartEventCard from './CartEventCard';
+
+const formattedOrderTotal = orderTotal => (orderTotal / 100).toFixed(2);
 
 const Cart = () => {
     const events = useSelector(selectTickets);
-    const dispatch = useDispatch();
+
+    const [orderTotal] = useState(Object.keys(events).reduce((acc, e) => acc + events[e].tickets.reduce((_acc, t) => _acc + t.price, 0), 0));
 
     return (
-        <div id="Cart">
-            {Object.keys(events).length && Object.keys(events).map((eventId, key) => {
-                const event = events[eventId];
-                return (
-                    <div className="floating-card" key={key}>
-                        <div className="card-header">
-                            <h2>{event.title}</h2>
-                            <span><small>@ {event.venueByVenue.name}</small></span>
-                            <span className="event-total">${(event.tickets.reduce((acc, ticket) => acc + ticket.price, 0) / 100).toFixed(2)}</span>
-                        </div>
-                        <div className="card-body">
-                            <div className="address-details">
-                                <p>{DateFormat(event.date)}</p>
-                                <p>{event.addressByAddress.address_1}</p>
-                                {event.addressByAddress.address_2 && <p>{event.addressByAddress.address_2}</p>}
-                                <p>{event.addressByAddress.city} {event.addressByAddress.state}, {event.addressByAddress.zip}</p>
-                            </div>
-                            <div className="hr"></div>
-                            <ul>
-                                {events[eventId].tickets.length && events[eventId].tickets.map((ticket, ticketKey) => (
-                                    <li key={`${key}:${ticketKey}`}>
-                                        <p>x1 ticket @ ${(ticket.price / 100).toFixed(2)}</p>
-                                        <button onClick={() => dispatch(removeTicket({ eventId }))}><i className="icon-trash"></i></button>
-                                    </li>
-                                ))}
-                            </ul>
-                        </div>
+        <div id="Cart" className="columns">
+            <div className="cart-details column is-two-thirds">
+                <CartEventCard events={events} />
+            </div>
+            <div className="cart-summary column is-one-third">
+                <div className="floating-card">
+                    <div className="card-header">
+                        <span>Cart Summary</span>
                     </div>
-                )
-            }
-            )}
+                    <div className="card-body">
+                        <p>
+                            <span>Subtotal: </span>
+                            <span className="order-subtotal">${formattedOrderTotal(orderTotal)}</span>
+                        </p>
+                        <p>
+                            <span>Taxes: </span>
+                            <span className="taxesTotal">${formattedOrderTotal(orderTotal * 0.3)}</span>
+                        </p>
+                        <div className="hr"></div>
+                        <p className="order-total">
+                            <span>Total: </span>
+                            <span>${formattedOrderTotal(orderTotal + (orderTotal * 0.3))}</span>
+                        </p>
+                    </div>
+                    <div className="card-footer">
+                        <button className="btn-success"><span className="header-font-1">PAY NOW</span></button>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 };
---
+++ src/components/CartEventCard.js
@@ -0,0 +1,42 @@
+import React, { Fragment } from 'react';
+import { useDispatch } from 'react-redux';
+import { removeTicket } from '../store/ticketsSlice';
+import { DateFormat } from '../utils/dates';
+    
+export default function CartEventCard ({ events }) {
+    const dispatch = useDispatch();
+
+    return (
+        <Fragment>
+            {Object.keys(events).length && Object.keys(events).map((eventId, key) => {
+                const event = events[eventId];
+                return (
+                    <div className="card" key={key}>
+                        <div className="card-header">
+                            <h2>{event.title}</h2>
+                            <span><small>@ {event.venueByVenue.name}</small></span>
+                            <span className="event-total">${(event.tickets.reduce((acc, ticket) => acc + ticket.price, 0) / 100).toFixed(2)}</span>
+                        </div>
+                        <div className="card-body">
+                            <div className="address-details">
+                                <p>{DateFormat(event.date)}</p>
+                                <p>{event.addressByAddress.address_1}</p>
+                                {event.addressByAddress.address_2 && <p>{event.addressByAddress.address_2}</p>}
+                                <p>{event.addressByAddress.city} {event.addressByAddress.state}, {event.addressByAddress.zip}</p>
+                            </div>
+                            <div className="hr"></div>
+                            <ul>
+                                {events[eventId].tickets.length && events[eventId].tickets.map((ticket, ticketKey) => (
+                                    <li key={`${key}:${ticketKey}`}>
+                                        <p>x1 ticket @ ${(ticket.price / 100).toFixed(2)}</p>
+                                        <button onClick={() => dispatch(removeTicket({ eventId }))}><i className="icon-trash"></i></button>
+                                    </li>
+                                ))}
+                            </ul>
+                        </div>
+                    </div>
+                )
+            })}
+        </Fragment>
+    );
+}
