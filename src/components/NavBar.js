2c2e6da019c79c3efd94280c5b3e5a672fb237d1
Nicholas Arcuri <narcuri1@gmail.com>
2020-04-08 14:29:57 -0400

    Update App Name

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,10 +2,12 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import UserElements from './UserElements';
 
+const { REACT_APP_SITE_NAME: SITE_NAME } = process.env;
+
 const NavBar = () => (
     <header>
         <Link to="/">
-            <h1>Ticketing</h1>
+            <h1>{SITE_NAME}</h1>
         </Link>
         <UserElements />
     </header>
