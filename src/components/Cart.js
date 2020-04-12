8c67a644074d9369803a852ae3bcf2b2d13d4a8a
Nicholas Arcuri <narcuri1@gmail.com>
2020-04-11 21:14:01 -0400

    Fix cart page columns wrapper

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -17,35 +17,37 @@ const Cart = () => {
     if (!order || !Object.keys(order.events).length) return <p>Cart is empty</p>;
 
     return (
-        <div id="Cart" className="columns">
-            <div className="cart-details column is-two-thirds">
-                <CartEventCard events={order.events} />
-            </div>
-            <div className="mobile-indicator">
-                <span className="icon-arrow-down"></span>
-            </div>
-            <div className="cart-summary column is-one-third">
-                <div className="floating-card">
-                    <div className="card-header">
-                        <h2>Cart Summary</h2>
-                    </div>
-                    <div className="card-body">
-                        <p>
-                            <span>Subtotal: </span>
-                            <span className="order-subtotal">${formatDollars(orderTotal)}</span>
-                        </p>
-                        <p>
-                            <span>Taxes: </span>
-                            <span className="taxesTotal">${formatDollars(orderTotal * 0.3)}</span>
-                        </p>
-                        <div className="hr"></div>
-                        <p className="order-total">
-                            <span>Total: </span>
-                            <span>${formatDollars(orderTotal + (orderTotal * 0.3))}</span>
-                        </p>
-                    </div>
-                    <div className="card-footer">
-                        <CheckoutForm />
+        <div id="Cart">
+            <div className="page-wrap columns">
+                <div className="cart-details column is-two-thirds">
+                    <CartEventCard events={order.events} />
+                </div>
+                <div className="mobile-indicator">
+                    <span className="icon-arrow-down"></span>
+                </div>
+                <div className="cart-summary column is-one-third">
+                    <div className="floating-card">
+                        <div className="card-header">
+                            <h2>Cart Summary</h2>
+                        </div>
+                        <div className="card-body">
+                            <p>
+                                <span>Subtotal: </span>
+                                <span className="order-subtotal">${formatDollars(orderTotal)}</span>
+                            </p>
+                            <p>
+                                <span>Taxes: </span>
+                                <span className="taxesTotal">${formatDollars(orderTotal * 0.3)}</span>
+                            </p>
+                            <div className="hr"></div>
+                            <p className="order-total">
+                                <span>Total: </span>
+                                <span>${formatDollars(orderTotal + (orderTotal * 0.3))}</span>
+                            </p>
+                        </div>
+                        <div className="card-footer">
+                            <CheckoutForm />
+                        </div>
                     </div>
                 </div>
             </div>
